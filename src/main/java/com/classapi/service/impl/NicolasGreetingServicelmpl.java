d0ef58843144995ad66668d33b3da7aa79f5a757
nicolasG27 <ngm12199@gmail.com>
2026-03-11 17:52:00 -0700

    Se cambiaron los mensajes de BastianGreetingService. Hola soy Mateo. Se agregó mateo. Se hizo mi Greeting

---
+++ src/main/java/com/classapi/service/impl/NicolasGreetingServicelmpl.java
@@ -0,0 +1,22 @@
+package com.classapi.service.impl;
+
+import com.classapi.service.GreetingService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+import static java.time.LocalTime.now;
+
+@Service("bastian")
+@Slf4j
+public class NicolasGreetingServicelmpl implements GreetingService {
+
+    @Override
+    public String greeting() {
+        log.info("¡Hola, soy Nico");
+        log.info("Mateo Guapo, Hola hola hola");
+        log.info("Este mensaje está en mi propio servicio. Ya no");
+        return "MATEOOOOOOOO ¡Hola, soy Nicolás, y la hora de hoy es " + now() + "!. Tengo sueño";
+    }
+}
