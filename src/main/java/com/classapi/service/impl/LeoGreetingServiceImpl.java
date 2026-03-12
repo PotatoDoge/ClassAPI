f04740b83a07366b0a9e219f388b3d92f63f54d2
lpiedras4 <piedras.leo@gmail.com>
2026-03-11 17:25:47 -0700

    Se creo servicio de saludo

---
+++ src/main/java/com/classapi/service/impl/LeoGreetingServiceImpl.java
@@ -0,0 +1,17 @@
+package com.classapi.service.impl;
+
+import com.classapi.service.GreetingService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+
+import static java.time.LocalTime.now;
+
+@Service("leo")
+@Slf4j public class LeoGreetingServiceImpl implements GreetingService {
+    @Override public String greeting(){
+     log.info("Hola soy Leo!");
+     log.info("Este mensaje es para probar mi servicio creado");
+     return "Q onda soy Leo y la fecha de hoy es: " + now();
+    }
+}
