72c3b1ae3d80b03f6274d4e81329fad9e51ed62e
bryan <jeronimo.bryan@cetry.edu.mxgit config --global user.name>
2026-03-11 17:24:49 -0700

    mi pan sususú sususú mi pa-a-a-a-a-an...Bryan

---
+++ src/main/java/com/classapi/service/impl/BryanGreetingServiceImpl.java
@@ -0,0 +1,16 @@
+package com.classapi.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import static java.time.LocalTime.now;
+
+@Service("Bryan")
+@Slf4j
+public class BryanGreetingServiceImpl {
+    public String greeting(){
+        log.info("Que tal soy sech");
+        log.info("Este mensaje esta en mi porpio servicio");
+        return "que tal soy sech y la hora de hoy es :" + now() + "";
+    }
+}
